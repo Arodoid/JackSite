a7ebd6b7abac59d1c6c8672d18795e55944b721a
Jack Richards <jackrichards@me.com>
2025-04-11 12:56:30 -0700

    Fix path to statics.js for Linux/CI build

--- compute-js/src/index.js
+++ compute-js/src/index.js
@@ -1,11 +1,10 @@
 /// <reference types="@fastly/js-compute" />
-import { getServer } from '..\static-publisher/statics.js';
+import { getServer } from "../static-publisher/statics.js";
 const staticContentServer = getServer();
 
 // eslint-disable-next-line no-restricted-globals
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 async function handleRequest(event) {
-
   const response = await staticContentServer.serveRequest(event.request);
   if (response != null) {
     return response;
@@ -14,5 +13,5 @@ async function handleRequest(event) {
   // Do custom things here!
   // Handle API requests, serve non-static responses, etc.
 
-  return new Response('Not found', { status: 404 });
+  return new Response("Not found", { status: 404 });
 }
